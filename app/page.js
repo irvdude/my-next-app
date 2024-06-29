04f5cfba0a4217e4552bc60efc65438ea1f98223
Irvin <irvdude44@gmail.com>
2024-06-29 19:50:27 -0400

    small edit to page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 		<main className={styles.main}>
 			<div className={styles.description}>
 				<p>
-					Get coding by editing&nbsp;
+					Start blazing by editing &nbsp;
 					<code className={styles.code}>app/page.js</code>
 				</p>
 				<div>
